7a158abe2803d8c2d99494d9d684de256f761714
Tikaraj Ghising <erghising@gmail.com>
2021-08-09 18:03:08 +1000

    Server created for data visualization

---
+++ server-data-visualization/src/main/java/com/openinsight/serverdatavisualization/ServerDataVisualizationApplication.java
@@ -0,0 +1,13 @@
+package com.openinsight.serverdatavisualization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerDataVisualizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerDataVisualizationApplication.class, args);
+	}
+
+}
